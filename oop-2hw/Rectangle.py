2629d800a5ce529a262c1182be43d9e54089c1a3
korotkovaliza <74649581+korotkovaliza@users.noreply.github.com>
2023-12-08 14:28:15 +0300

    Update Rectangle.py

--- oop-2hw/Rectangle.py
+++ oop-2hw/Rectangle.py
@@ -7,29 +7,29 @@ from matplotlib.patches import Rectangle as RectanglePatch
 class Rectangle(Figure):
     def __init__(self, points):
         super().__init__(points)
-        self.points = points
-        self.square = self.calculateSquare()
-        self.perimeter = self.calculatePerimeter()
         
     def calculatePerimeter(self):
-        return 2 * (abs(self.points[0] - self.points[2]) + abs(self.points[1] - self.points[3]))
+        return 2 * (abs(self.points[0][0] - self.points[1][0]) + abs(self.points[0][1] - self.points[1][1]))
 
     def calculateSquare(self):
-        return abs(self.points[0] - self.points[2]) * abs(self.points[1] - self.points[3])
+        return abs(self.points[0][0] - self.points[1][0]) * abs(self.points[0][1] - self.points[1][1])
     
     def __str__(self):
         return f"['rectangle', {self.square}, {self.perimeter}, [{self.points[:2]}, {self.points[2:]}]]"
 
     @classmethod
     def constructFromSeries(cls, series: pd.Series):
-        return cls([float(value) for value in series[1].split(':')] + [float(value) for value in series[2].split(':')])
+        return cls([[float(value) for value in series[1].split(':')]] +[[float(value) for value in series[2].split(':')]])
 
     def graphFigure(self):
         fig, ax = plt.subplots()
-        rectangle = RectanglePatch((self.points[0], self.points[1]), abs(self.points[0] - self.points[2]), abs(self.points[1] - self.points[3]), fill=False)
+        rectangle = RectanglePatch((self.points[0][0], self.points[0][1]), abs(self.points[0][0] - self.points[1][0]), abs(self.points[0][1] - self.points[1][1]), fill=False)
         ax.add_artist(rectangle)
         ax.set_aspect('equal')
-        plt.xlim(min(self.points) - 1, max(self.points) + 1)
-        plt.ylim(min(self.points) - 1, max(self.points) + 1)
-        plt.show()
+        
+        x_values = [point[0] for point in self.points] 
+        y_values = [point[1] for point in self.points]  
 
+        plt.xlim(min(x_values) - 1, max(x_values) + 1)
+        plt.ylim(min(y_values) - 1, max(y_values) + 1)
+        plt.show()
