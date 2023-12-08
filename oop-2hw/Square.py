b2e3c326b7c46987446357433f82cc05d5c82d77
korotkovaliza <74649581+korotkovaliza@users.noreply.github.com>
2023-12-08 14:27:47 +0300

    Update Square.py

--- oop-2hw/Square.py
+++ oop-2hw/Square.py
@@ -1,4 +1,3 @@
-
 import pandas as pd
 from Figure import Figure
 import math
@@ -8,35 +7,34 @@ from matplotlib.patches import Rectangle as RectanglePatch
 class Square(Figure):
     def __init__(self, points):
         super().__init__(points)
-        self.points = points
-        self.square = self.calculateSquare()
-        self.perimeter = self.calculatePerimeter()
         
 
     def calculatePerimeter(self):
-        return 4*self.points[2]
+        return 4*self.points[1]
 
 
     def calculateSquare(self):
-        return pow(self.points[2],2)
+        return pow(self.points[1],2)
 
     
     def __str__(self):
-        return f"['square', {self.square}, {self.perimeter}, [{self.points[:2]}, {self.points[2]}]]"
+        return f"['square', {self.square}, {self.perimeter}, [{self.points[0]}, {self.points[1]}]]"
 
     @classmethod
  
     def constructFromSeries(cls,series: pd.Series):
-        return cls([float(value) for value in series[1].split(':')] + [float(series[2])])
+        return cls([[float(value) for value in series[1].split(':')]] + [float(series[2])])
 
 
     def graphFigure(self):
         fig, ax = plt.subplots()
-        rectangle = RectanglePatch((self.points[0], self.points[1]), self.points[2], self.points[2], fill=False)
+        rectangle = RectanglePatch((self.points[0][0], self.points[0][1]), self.points[1], self.points[1], fill=False)
         ax.add_artist(rectangle)
         ax.set_aspect('equal')
-        plt.xlim(self.points[0] - self.points[2] - 1, self.points[0] + self.points[2] + 1)
-        plt.ylim(self.points[1] - self.points[2] - 1, self.points[1] + self.points[2] + 1)
+        plt.xlim(self.points[0][0] - self.points[1] - 1, self.points[0][0] + self.points[1] + 1)
+        plt.ylim(self.points[0][1] - self.points[1] - 1, self.points[0][1] + self.points[1] + 1)
         plt.show()
+
+
 
 
